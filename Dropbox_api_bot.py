9ca661e238ac3bdbd57216ae283bf3c914e0cea9
typhus148 <prhciv@yahoo.com>
2016-07-07 11:04:43 -0400

    Changed name to files for clearer understanding of their function. Minor changes to dropbox_api_bot.py but still not functional.

--- Dropbox_api_bot.py
+++ Dropbox_api_bot.py
@@ -1,12 +1,13 @@
 import dropbox
-from Dropbox_api_auth import App_secret, App_key, Access_token
+from Dropbox_api_auth import Access_token
+# App_secret, App_key,
 
 
-def Dropbox_api_app(data):
-    flow = dropbox.client.DropboxOAuth2FlowNoRedirect(App_key, App_secret)
-    client = dropbox.Dropbox(Access_token)
-    # print('linked account: ', client.users_get_current_account())
-    f = open('fetched_lupus_tweets.txt', 'a')
+def Dropbox_api_app():
+    # flow = dropbox.client.DropboxOAuth2FlowNoRedirect(App_key, App_secret)
     # client.files_create_folder('/test_folder'
-    # client.files_upload(f, '/fetched_lupus_tweets.txt')
-    client.files_upload_session_append_v2(f, data, close=False)
+    # print('linked account: ', client.users_get_current_account())
+    client = dropbox.Dropbox(Access_token)
+    f = open('fetched_lupus_tweets.txt', 'rb')
+    client.files_upload(f, '/fetched_lupus_tweets.txt')
+    # client.files_upload_session_append_v2(f, cursor=, close=False)
